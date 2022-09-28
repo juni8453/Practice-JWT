33d805173094f45aeb9d8faec06e05f12d034b0e
ByeoungJoon Jeon <juni8453@naver.com>
2022-09-28 20:59:18 +0900

    :sparkles: 유저 도메인 및 컨트롤러 생성
    # Description
    - 간단한 JPA 엔티티 기반 유저 객체와 Controller 생성

---
+++ src/main/java/com/cos/jwt/model/User.java
@@ -0,0 +1,44 @@
+package com.cos.jwt.model;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String roles; // USER, ADMIN
+
+    public List<String> getRoleList() {
+        if (this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+
+        return new ArrayList<>();
+    }
+
+    @Builder
+    public User(Long id, String username, String password, String roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+}
